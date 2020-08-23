59898f0fa0d5b099399355c7f298a78c82924eb1
Vakon <jamespagedev@outlook.com>
2020-08-23 05:47:14 -0500

    home page background

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="div-background-window" />
+      <div className="div-home-bottom" />
+    </div>
+  );
+}
+
+export default App;
