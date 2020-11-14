d25e31a5306f38caa665b4d27177ba6f3414dd53
Vakon <jamespagedev@outlook.com>
2020-11-14 01:36:12 -0600

    made into bouncing arrow responsive

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -16,20 +16,11 @@ function Intro() {
         to={`about`}
         spy={true}
         smooth={true}
-        offset={-80}
+        offset={-30}
         duration={500}
         className="a-intro-bottom-click"
       >
         <div className="div-bouncing-arrow" />
-        {/* <div className="div-intro-bottom" /> */}
-        {/* <div className="div-intro-bottom">
-          <svg width="0" height="0">
-            <clipPath id="myclippath" clipPathUnits="objectBoundingBox">
-              <polygon points="0.50 0.50, 1.00 0, 1.00 1.00, 0 1.00, 0 0"/>
-            </clipPath>
-          </svg>
-        </div> */}
-        {/* <div style={{position: "absolute", bottom: "0", width: "100%", height: "100%", backgroundColor: "#ffffff"}} data-polyclip="50%, 50%, 100%, 0, 100%, 100%, 0, 100%, 0,0" /> */}
         <div className="div-left-triangle" />
         <div className="div-right-triangle" />
       </Link>
